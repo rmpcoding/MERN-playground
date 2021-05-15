882d29df6e006c961e8e632225a02295c6258c8f
Robert Parsons <rmpcoding@gmail.com>
2021-05-15 06:08:52 -0700

    Adds fixed directory path

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static("client/build"))
 
 if (process.env.NODE_ENV === "production") {
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+        res.sendFile(path.join(__dirname, "client/build/index.html"))
     })
 }
 
