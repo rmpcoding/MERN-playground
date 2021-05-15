f6e4c4b856f551fb1bd998e7fa8f43d47be8dd7c
Robert Parsons <rmpcoding@gmail.com>
2021-05-15 06:24:32 -0700

    Adds middleware to server

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("client/build"))
 
 if (process.env.NODE_ENV === "production") {
