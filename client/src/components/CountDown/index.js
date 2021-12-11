7631b0b542e4071b1917849670c5cb23f4100e7f
Robert Parsons <rmpcoding@gmail.com>
2021-12-11 15:37:50 -0800

    Adds setInterval timer to app

--- client/src/components/CountDown/index.js
+++ client/src/components/CountDown/index.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react';
 
 const CountDown = () => {
+    const [time, setTime] = useState(0);
 
+    useEffect(() => {
+        const timer = setInterval(() => {
+            console.log(time)
+            setTime(time => ++time);
+        }, 1000);
+        return () => clearInterval(timer)
+    }, [time]);
 
     return (
         <>
-
+            <p>{time}</p>
         </>
     );
 };
